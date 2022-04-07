b2c1c290edc9de02ed5d7e4b43663f1737391a75
Dionis <den70007.ua@gmail.com>
2022-04-07 15:55:25 +0300

    added letter handler for inboxkitten

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ from temp_mail import InboxKittenMail
 mail = InboxKittenMail()
 mail.set_mail('heavy-nobody-46')
 
+
+@mail.letter_handler(sender_mail='den70007.ua@gmail.com')
+def test(letter):
+    print(letter)
+
+
 mail.poling()
--- temp_mail/inboxkitten/__init__.py
+++ temp_mail/inboxkitten/__init__.py
@@ -1,3 +1,5 @@
+import re
+from datetime import datetime
 import time
 import requests
 from temp_mail.inboxkitten.letter import Letter
@@ -9,7 +11,7 @@ class InboxKittenMail:
     def __init__(self, proxy=None):
         self.__mail = None
         self.__proxies = {'http': proxy, 'https': proxy} if proxy else None
-        self.__handles = []
+        self.__handlers = []
 
     def get_mail(self):
         self.set_mail(random_string())
@@ -24,9 +26,27 @@ class InboxKittenMail:
 
         r = requests.get(f'https://inboxkitten.com/api/v1/mail/list?recipient={self.__mail}', proxies=self.__proxies)
         if r.status_code == 200:
-            letters = [Letter(__letter['storage']['url'], __letter['message']['headers'], __letter['timestamp'], self.__proxies) for __letter in r.json()]
+            letters = [Letter(__letter['storage']['url'], __letter['message']['headers'], __letter['timestamp'],
+                              self.__proxies) for __letter in r.json()]
             return letters
         return []
+
+    def letter_handler(self, sender_name='', sender_mail='', subject='', re_subject='', only_new: bool = True):
+        def wrapper(handler):
+            self.__handlers.append({'handler': handler, 'sender_name': sender_name, 'sender_mail': sender_mail,
+                                    'subject': subject, 're_subject': re_subject,
+                                    'time': datetime.now() if only_new else None})
+
+        return wrapper
+
+    def __letter_handler(self, _letter):
+        for handler in self.__handlers:
+            if (not handler['sender_name'] or handler['sender_name'] == _letter.sender_name) and \
+                    (not handler['sender_mail'] or handler['sender_mail'] == _letter.sender_mail) and \
+                    (not handler['subject'] or handler['subject'] == _letter.subject) and \
+                    (not handler['re_subject'] or re.findall(handler['re_subject'], _letter.subject)) and \
+                    (not handler['time'] or handler['time'] < _letter.send_time):
+                handler['handler'](_letter)
 
     def poling(self, timeout: int = 10):
         letters = []
@@ -35,8 +55,7 @@ class InboxKittenMail:
                 for _letter in self.get_inbox():
                     if hash(_letter) not in letters:
                         letters.append(hash(_letter))
-                        print(_letter)
-
+                        self.__letter_handler(_letter)
                 time.sleep(timeout)
         except KeyboardInterrupt:
             pass
