00759adad50c77443ee6c4d5d8de320fbe9b8403
Dionis <den70007.ua@gmail.com>
2022-04-08 08:17:31 +0300

    Update long_description and another bugs

--- account_generator_helper/__init__.py
+++ account_generator_helper/__init__.py
@@ -5,4 +5,4 @@ from .fake_data.utilities import *
 
 __author__ = 'Dionis1902'
 __email__ = "den70007.ua@gmail.com"
-__version__ = '1.0.0'
+__version__ = '1.0.1'
--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
     name='account_generator_helper',
-    version='1.0.0',
+    version='1.0.1',
     packages=['account_generator_helper', 'account_generator_helper.fake_data', 'account_generator_helper.temp_mail',
               'account_generator_helper.temp_mail.mail', 'account_generator_helper.temp_mail.gmailnator',
               'account_generator_helper.temp_mail.inboxkitten', 'account_generator_helper.temp_mail.tempmailplus',
@@ -16,5 +19,7 @@ setup(
     author='Dionis1902',
     author_email='den70007.ua@gmail.com',
     description='This library is helpful when creating accounts, it has everything you need for this',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=required
 )
