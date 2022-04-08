414425911c415228fe650f2bafb26c525b4f033b
Dionis <den70007.ua@gmail.com>
2022-04-08 08:38:51 +0300

    Fix bugs with workflows

--- account_generator_helper/__init__.py
+++ account_generator_helper/__init__.py
@@ -4,5 +4,5 @@ from .fake_data import *
 from .fake_data.utilities import *
 
 __author__ = 'Dionis1902'
-__email__ = "den70007.ua@gmail.com"
+__email__ = 'den70007.ua@gmail.com'
 __version__ = '1.0.0'
