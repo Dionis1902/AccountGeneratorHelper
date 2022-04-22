baa029b4d02b8b9bd7edf609c752674d5a599002
Dionis <den70007.ua@gmail.com>
2022-04-22 16:43:33 +0300

    fix version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ required = (this_directory / 'requirements.txt').read_text().splitlines()
 
 setup(
     name='account_generator_helper',
-    version='1.0.7',
+    version='1.0.8',
     packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
