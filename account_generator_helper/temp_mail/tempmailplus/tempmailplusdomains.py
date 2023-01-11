ef83137ef212a965d55fb8d2069add05b6d79afc
Denis <63014993+Dionis1902@users.noreply.github.com>
2023-01-11 20:09:25 +0200

    Update tempmailplusdomains.py

--- account_generator_helper/temp_mail/tempmailplus/tempmailplusdomains.py
+++ account_generator_helper/temp_mail/tempmailplus/tempmailplusdomains.py
@@ -5,7 +5,6 @@ class TempMailPlusDomains(enum.Enum):
     MAILTO_PLUS = 'mailto.plus'
     FEXPOST_COM = 'fexpost.com'
     FEXBOX_ORG = 'fexbox.org'
-    FEXBOX_RU = 'fexbox.ru'
     MAILBOX_IN_UA = 'mailbox.in.ua'
     ROVER_INFO = 'rover.info'
     INPWA_COM = 'inpwa.com'
