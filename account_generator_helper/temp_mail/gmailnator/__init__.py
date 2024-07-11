44853d76bd7ad960d9b94a6c1d3cb6318a10f76a
Dor Segal <dor982@gmail.com>
2024-07-11 14:40:47 +0200

    fix url emailinator

--- account_generator_helper/temp_mail/gmailnator/__init__.py
+++ account_generator_helper/temp_mail/gmailnator/__init__.py
@@ -5,24 +5,24 @@ from .letter import Letter
 from urllib.parse import unquote
 
 headers = {
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'content-type': 'application/json'
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "content-type": "application/json",
 }
 
 
 class GmailNator(Mail):
     """
-    Class for work with https://www.emailnator.com/
+    Class for work with https://emailnator.com/
     """
 
     def __init__(self, proxy=None):
         super().__init__(proxy)
-        r = self._s.get('https://www.emailnator.com/')
+        r = self._s.get("https://emailnator.com/")
         if r.status_code == 403:
             raise CloudflareDetect()
 
     def __get_xsrf_token(self):
-        token = self._s.cookies.get('XSRF-TOKEN', '')
+        token = self._s.cookies.get("XSRF-TOKEN", "")
         if not token:
             raise CloudflareDetect()
         return unquote(token)
@@ -39,12 +39,23 @@ class GmailNator(Mail):
         :param use_point:  Generate email with "." in address.
         :return: Random email address.
         """
-        data = ['domain', 'plusGmail', 'dotGmail']
-        payload = json.dumps({'email': [i for i, k in zip(data, [use_custom_domain, use_plus, use_point]) if k]})
-        r = self._s.post('https://www.emailnator.com/generate-email',
-                         headers={**headers, 'x-xsrf-token': self.__get_xsrf_token()}, data=payload)
+        data = ["domain", "plusGmail", "dotGmail"]
+        payload = json.dumps(
+            {
+                "email": [
+                    i
+                    for i, k in zip(data, [use_custom_domain, use_plus, use_point])
+                    if k
+                ]
+            }
+        )
+        r = self._s.post(
+            "https://emailnator.com/generate-email",
+            headers={**headers, "x-xsrf-token": self.__get_xsrf_token()},
+            data=payload,
+        )
         if r.status_code == 200:
-            return self._set_email(r.json()['email'][0])
+            return self._set_email(r.json()["email"][0])
         raise ProblemWithGetEmail()
 
     def set_email(self, email):
@@ -54,10 +65,16 @@ class GmailNator(Mail):
         return self._set_email(email)
 
     def get_inbox(self):
-        payload = json.dumps({'email': self._email})
-        r = self._s.post('https://www.emailnator.com/message-list',
-                         headers={**headers, 'x-xsrf-token': self.__get_xsrf_token()}, data=payload)
+        payload = json.dumps({"email": self._email})
+        r = self._s.post(
+            "https://emailnator.com/message-list",
+            headers={**headers, "x-xsrf-token": self.__get_xsrf_token()},
+            data=payload,
+        )
         if r.status_code == 200:
-            return [Letter(self._email, _letter, self.__get_xsrf_token(), self._s) for _letter in r.json()['messageData'] if
-                    'ADS' not in _letter['messageID']]
+            return [
+                Letter(self._email, _letter, self.__get_xsrf_token(), self._s)
+                for _letter in r.json()["messageData"]
+                if "ADS" not in _letter["messageID"]
+            ]
         return []
--- setup.py
+++ setup.py
@@ -3,45 +3,45 @@ from pathlib import Path
 
 this_directory = Path(__file__).parent
 
-long_description = (this_directory / 'README.md').read_text()
-required = (this_directory / 'requirements.txt').read_text().splitlines()
+long_description = (this_directory / "README.md").read_text()
+required = (this_directory / "requirements.txt").read_text().splitlines()
 
 setup(
-    name='account_generator_helper',
-    version='1.0.14',
+    name="account_generator_helper",
+    version="1.0.14",
     packages=find_packages(),
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Communications :: Email',
-        'Topic :: Utilities',
-        'Topic :: Internet :: Proxy Servers',
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Communications :: Email",
+        "Topic :: Utilities",
+        "Topic :: Internet :: Proxy Servers",
     ],
     project_urls={
-        'Source': 'https://github.com/Dionis1902/AccountGeneratorHelper/tree/main/account_generator_helper',
-        'Examples': 'https://github.com/Dionis1902/AccountGeneratorHelper/tree/main/examples',
-        'Tracker': 'https://github.com/Dionis1902/AccountGeneratorHelper/issues',
+        "Source": "https://github.com/Dionis1902/AccountGeneratorHelper/tree/main/account_generator_helper",
+        "Examples": "https://github.com/Dionis1902/AccountGeneratorHelper/tree/main/examples",
+        "Tracker": "https://github.com/Dionis1902/AccountGeneratorHelper/issues",
     },
-    url='https://github.com/DioniS1902/AccountGeneratorHelper',
-    license='MIT',
-    author='Dionis1902',
-    author_email='den70007.ua@gmail.com',
-    description='This library is helpful when creating accounts, it has everything you need for this',
+    url="https://github.com/DioniS1902/AccountGeneratorHelper",
+    license="MIT",
+    author="Dionis1902",
+    author_email="den70007.ua@gmail.com",
+    description="This library is helpful when creating accounts, it has everything you need for this",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    python_requires='>=3',
-    keywords='python email captcha proxy pypi sms gmail temp-mail captcha-solving captcha-solver fake-data account-generator captcha-recognition '
-             'fake-data-generator free-proxy recive-sms gmailnator gmailnator-api poxies',
-    install_requires=required
+    long_description_content_type="text/markdown",
+    python_requires=">=3",
+    keywords="python email captcha proxy pypi sms gmail temp-mail captcha-solving captcha-solver fake-data account-generator captcha-recognition "
+    "fake-data-generator free-proxy recive-sms gmailnator gmailnator-api poxies",
+    install_requires=required,
 )
