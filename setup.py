61e1f060a481a2d16a81bb4721c3a1bbba258397
Dor Segal <dor982@gmail.com>
2024-07-16 10:44:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ required = (this_directory / "requirements.txt").read_text().splitlines()
 
 setup(
     name="account_generator_helper",
-    version="1.0.14",
+    version="1.0.15",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
